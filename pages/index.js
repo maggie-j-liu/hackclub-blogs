5558313d503a75b8292931fa74609b4bdacccaa4
maggie-j-liu <maggie.j.liu@gmail.com>
2022-04-04 12:13:30 -0700

    supabase + slack sign in

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
+import { useEffect } from "react";
+import supabase from "../utils/supabase";
+
 export default function Home() {
-  return <div>hack club blogs</div>;
+  useEffect(() => {
+    console.log(supabase.auth.session());
+  }, []);
+  const signInWithSlack = async () => {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: "slack",
+    });
+    console.log(user, session, error);
+  };
+  return (
+    <div>
+      <button
+        onClick={async (e) => {
+          e.preventDefault();
+          await signInWithSlack();
+        }}
+        type="button"
+      >
+        Sign In With Slack
+      </button>
+    </div>
+  );
 }
