70b8fce66776154d30dbd16401b11a1f2cc568bd
exu <git@ella.cx>
2022-04-09 12:26:26 -0700

    Make gravatar Slack avatars work

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["avatars.slack-edge.com"],
+    domains: ["avatars.slack-edge.com", "secure.gravatar.com", "github.com"],
   },
 };
 
