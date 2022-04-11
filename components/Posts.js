d92b18240b50d24438eeb866fc8069752b225da3
maggie-j-liu <maggie.j.liu@gmail.com>
2022-04-10 22:05:35 -0700

    show written blogs on home page

--- components/Posts.js
+++ components/Posts.js
@@ -1,20 +1,90 @@
 import Post from "./Post";
 import { useState } from "react";
+import supabase from "../utils/supabase";
 
-const Posts = ({ posts }) => {
-  const POST_INCREMENT = 20;
+export const POST_INCREMENT = 20;
+const Posts = ({ allPosts, rssPosts, postsRetrieved }) => {
+  const [blogPostsRetrieved, setBlogPostsRetrieved] = useState(postsRetrieved);
+  const [blogPostsShown, setBlogPostsShown] = useState(() => {
+    let num = 0;
+    for (const post of allPosts) {
+      if (!post.fromRSS) {
+        num++;
+      }
+    }
+    return num;
+  });
   const [numPostsToShow, setNumPostsToShow] = useState(
-    Math.min(POST_INCREMENT, posts.length)
+    Math.min(POST_INCREMENT, allPosts.length)
   );
-  const loadMore = () => {
-    setNumPostsToShow((p) => Math.min(p + POST_INCREMENT, posts.length));
+  const [posts, setPosts] = useState(allPosts);
+  const [noMorePosts, setNoMorePosts] = useState(false);
+  const loadMore = async () => {
+    if (noMorePosts) {
+      setNumPostsToShow((n) => Math.min(n + POST_INCREMENT, posts.length));
+    }
+    const numNeeded = POST_INCREMENT - (blogPostsRetrieved - blogPostsShown);
+    console.log("numNeeded", numNeeded);
+    const { data: newPosts } = await supabase
+      .from("posts")
+      .select(
+        `
+          slug,
+          title,
+          created_at,
+          author (
+            username,
+            slack_id
+          )
+        `
+      )
+      .order("created_at", { ascending: false })
+      .range(blogPostsRetrieved, blogPostsRetrieved + numNeeded - 1);
+    if (newPosts === null || newPosts.length === 0) {
+      setNoMorePosts(true);
+      setNumPostsToShow((n) => Math.min(n + POST_INCREMENT, posts.length));
+      return;
+    }
+    const formattedNewPosts = [];
+    for (const post of newPosts) {
+      const currentPost = {
+        title: post.title,
+        description: "",
+        link: `/post/${post.slug}`,
+        date: post.created_at,
+        blogTitle: `${post.author.username}'s Blog`,
+        author: post.author.username,
+        blogLink: `/user/${post.author.slack_id}`,
+        fromRSS: false,
+      };
+      formattedNewPosts.push(currentPost);
+    }
+    const newAllPosts = [...posts, ...formattedNewPosts];
+    newAllPosts.sort((a, b) => {
+      const aTime = new Date(a.date).getTime();
+      const bTime = new Date(b.date).getTime();
+      return bTime - aTime;
+    });
+
+    const newNumPosts = blogPostsRetrieved + rssPosts.length + newPosts.length;
+    const newNumShown = Math.min(numPostsToShow + POST_INCREMENT, newNumPosts);
+    let newNumBlogPostsShown = 0;
+    for (let i = 0; i < newAllPosts.length; i++) {
+      if (!newAllPosts[i].fromRSS) {
+        newNumBlogPostsShown++;
+      }
+    }
+    setBlogPostsShown(newNumBlogPostsShown);
+    setPosts(newAllPosts);
+    setNumPostsToShow(newNumShown);
+    setBlogPostsRetrieved((b) => b + newPosts.length);
   };
   return (
     <div className="mx-auto flex flex-col gap-10">
-      {posts.slice(0, numPostsToShow).map((post) => (
-        <Post post={post} key={post.link} />
-      ))}
-      {numPostsToShow < posts.length ? (
+      {posts.slice(0, numPostsToShow).map((post) => {
+        return <Post post={post} key={post.link} />;
+      })}
+      {noMorePosts && numPostsToShow === posts.length ? null : (
         <button
           type="button"
           onClick={(e) => {
@@ -25,7 +95,7 @@ const Posts = ({ posts }) => {
         >
           Load More Posts
         </button>
-      ) : null}
+      )}
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import supabase from "../utils/supabase";
 import useAuth from "../utils/useAuth";
 import Parser from "rss-parser";
-import Posts from "../components/Posts";
+import Posts, { POST_INCREMENT } from "../components/Posts";
 import Link from "next/link";
 import Layout from "../components/Layout";
 
-export default function Home({ posts }) {
+export default function Home({ allPosts, rssPosts, postsRetrieved }) {
   return (
     <Layout maxW="max-w-4xl" title="Home">
       <h1 className="mx-auto w-max bg-gradient-to-r from-red to-orange bg-clip-text pb-2 text-center text-6xl font-extrabold text-transparent">
@@ -14,7 +14,7 @@ export default function Home({ posts }) {
       <h2 className="mx-auto text-center text-2xl text-secondary">
         Check out blog posts written by Hack Clubbers!
       </h2>
-      {posts.length === 0 ? (
+      {allPosts.length === 0 ? (
         <p className="pt-8 text-center text-xl text-muted">
           No posts yet.{" "}
           <Link href="/new">
@@ -26,7 +26,11 @@ export default function Home({ posts }) {
       ) : (
         <>
           <div className="h-8" />
-          <Posts posts={posts} />
+          <Posts
+            allPosts={allPosts}
+            rssPosts={rssPosts}
+            postsRetrieved={postsRetrieved}
+          />
         </>
       )}
     </Layout>
@@ -35,10 +39,26 @@ export default function Home({ posts }) {
 
 export const getStaticProps = async () => {
   const { data: blogs } = await supabase.from("blogs").select();
+  const { data: posts } = await supabase
+    .from("posts")
+    .select(
+      `
+      slug,
+      title,
+      created_at,
+      author (
+        username,
+        slack_id
+      )
+    `
+    )
+    .order("created_at", { ascending: false })
+    .limit(POST_INCREMENT);
   const parser = new Parser();
   const parsePromises = blogs.map((blog) => parser.parseURL(blog.link));
   const feeds = await Promise.allSettled(parsePromises);
-  const posts = [];
+  const allPosts = [];
+  const rssPosts = [];
   for (let i = 0; i < feeds.length; i++) {
     const feed = feeds[i];
     if (feed.status === "rejected") {
@@ -46,7 +66,7 @@ export const getStaticProps = async () => {
       continue;
     }
     for (const item of feed.value.items) {
-      posts.push({
+      const currentPost = {
         title: item.title ?? "",
         description: item.contentSnippet ?? "",
         link: item.link ?? "",
@@ -54,10 +74,31 @@ export const getStaticProps = async () => {
         blogTitle: feed.value.title ?? "",
         author: item.creator ?? "",
         blogLink: feed.value.link ?? "",
-      });
+        fromRSS: true,
+      };
+      rssPosts.push(currentPost);
+      allPosts.push(currentPost);
     }
   }
-  posts.sort((a, b) => {
+  for (const post of posts) {
+    const currentPost = {
+      title: post.title,
+      description: "",
+      link: `/post/${post.slug}`,
+      date: post.created_at,
+      blogTitle: `${post.author.username}'s Blog`,
+      author: post.author.username,
+      blogLink: `/user/${post.author.slack_id}`,
+      fromRSS: false,
+    };
+    allPosts.push(currentPost);
+  }
+  rssPosts.sort((a, b) => {
+    const aTime = new Date(a.date).getTime();
+    const bTime = new Date(b.date).getTime();
+    return bTime - aTime;
+  });
+  allPosts.sort((a, b) => {
     const aTime = new Date(a.date).getTime();
     const bTime = new Date(b.date).getTime();
     return bTime - aTime;
@@ -65,7 +106,9 @@ export const getStaticProps = async () => {
 
   return {
     props: {
-      posts,
+      allPosts,
+      rssPosts,
+      postsRetrieved: posts.length,
     },
   };
 };
